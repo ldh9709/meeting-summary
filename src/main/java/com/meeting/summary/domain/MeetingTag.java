6e05c117dd2dec3a3f2a20e2b9e8c988d9cde819
ldh9709 <zszz5434@gmail.com>
2025-12-14 16:00:03 +0900

    docs : meetingTag 테이블 주석 추가

--- src/main/java/com/meeting/summary/domain/MeetingTag.java
+++ src/main/java/com/meeting/summary/domain/MeetingTag.java
@@ -1,7 +1,8 @@
 package com.meeting.summary.domain;
 
+/* 회의록과 태그 중간 테이블 */
 public class MeetingTag {
-	private Long meetingTagId;
-	private Long meetingId;
-	private Long tagId;
+	private Long meetingTagId;	//중간 테이블 고유 번호(SEQ)
+	private Long meetingId;		//회의록 번호(FK)
+	private Long tagId;			//태그 번호(FK)
 }
