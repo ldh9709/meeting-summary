a7367be4c556db8ca08910550e611fd9fc6cec4a
ldh9709 <zszz5434@gmail.com>
2025-12-21 22:51:57 +0900

    feat : tag 어노테이션 추가

--- src/main/java/com/meeting/summary/domain/Tag.java
+++ src/main/java/com/meeting/summary/domain/Tag.java
@@ -2,6 +2,15 @@ package com.meeting.summary.domain;
 
 import java.time.LocalDateTime;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data				//Getter, Setter
+@NoArgsConstructor	//기본 생성자(파라미터 없는 생성자) 생성 가능
+@AllArgsConstructor	//모든 필드를 파라미터로 받는 생성자 생성 가능 
+@Builder			//빌더 패턴 사용 가능
 /* 회의록 태그 정보 */
 public class Tag {
 	private Long tagId;					//태그 고유번호(SEQ)
